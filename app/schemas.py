19a9313aacc4c78cff17eabeef8968bf4a4698a3
Wicklife Oguda <ogudawicklife1@gmail.com>
2025-05-18 09:55:58 +0300

    Implement sensor data validation schemas

---
+++ app/schemas.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+
+from pydantic import BaseModel, Field
+
+
+class IncomingSensorData(BaseModel):
+    """
+    Validation schema for sensor data received by the API
+    """
+
+    device_id: str
+    temperature: float = Field(..., ge=-50, le=150)
+    humidity: float = Field(..., ge=0, le=100)
+    timestamp: datetime
+
+
+class OutgoingSensorData(IncomingSensorData):
+    """
+    Validation schema for sensor data sent by the API
+    """
+
+    id: int
+
+    class Config:
+        orm_mode = True
