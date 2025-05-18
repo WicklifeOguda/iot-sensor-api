b1ce8758c50ebd582f0a230a69927b15d1b03072
Wicklife Oguda <ogudawicklife1@gmail.com>
2025-05-18 11:23:41 +0300

    Implement SensorReading creation endpoint

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, BackgroundTasks, Depends
+from sqlalchemy.orm import Session
 
+from . import crud_actions, schemas
 from .database import SessionLocal
 
 router = APIRouter()
@@ -17,3 +19,16 @@ def get_db():
 @router.get("/health")
 def check_api_health():
     return {"status": "OK"}
+
+
+@router.post("/readings", status_code=202)
+def add_device_reading(
+    data: schemas.IncomingSensorData,
+    background_tasks: BackgroundTasks,
+    db: Session = Depends(get_db),
+):
+    """
+    SensorReading creation endpoint
+    """
+    background_tasks.add_task(crud_actions.save_reading, db, data)
+    return {"message": "Sensor reading received"}
