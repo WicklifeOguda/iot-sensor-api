61b244f27e28a4fdbbf8fd32edff54c1b061647e
Wicklife Oguda <ogudawicklife1@gmail.com>
2025-05-18 10:34:59 +0300

    Implement db injection function

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,17 @@
 from fastapi import APIRouter
 
+from .database import SessionLocal
+
 router = APIRouter()
+
+
+# DB injection function
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 @router.get("/health")
