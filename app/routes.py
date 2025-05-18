31d8bf3f531e1961c6833b89d02eef0a6674b99e
Wicklife Oguda <ogudawicklife1@gmail.com>
2025-05-18 11:46:00 +0300

    Implement SensorReading fetch endpoint

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from fastapi import APIRouter, BackgroundTasks, Depends
 from sqlalchemy.orm import Session
 
@@ -32,3 +34,17 @@ def add_device_reading(
     """
     background_tasks.add_task(crud_actions.save_reading, db, data)
     return {"message": "Sensor reading received"}
+
+
+@router.get("/readings/{device_id}", response_model=List[schemas.OutgoingSensorData])
+def fetch_device_readings(
+    device_id: str, limit: int = 10, db: Session = Depends(get_db)
+):
+    """
+    Fetch endpoint for getting specific device's readings
+    """
+    # control limit
+    if limit > 100:
+        limit = 100
+
+    return crud_actions.fetch_readings(db, device_id, limit)
